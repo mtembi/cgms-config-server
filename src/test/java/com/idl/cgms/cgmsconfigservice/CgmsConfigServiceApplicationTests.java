723cd7d125fcd05ef0dfd6ab46f0524b6a117177
Mtembi <mtembi@gmail.com>
2021-07-25 12:23:56 +0300

    Initial setup

---
+++ src/test/java/com/idl/cgms/cgmsconfigservice/CgmsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idl.cgms.cgmsconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgmsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
